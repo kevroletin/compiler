f5cdcf83ea63f3b38e3c095ad98210bf83324539
vkevroletin <kevroletin@gmail.com>
2010-12-09 22:51:05 +1000

    мелкие исправления в тестах

--- test1_scaner/test.sh
+++ test1_scaner/test.sh
@@ -5,7 +5,7 @@ for file in *.in
 do
 	num=$((num+1));
 	i=${file%%.in}
-	../bin/Debug/compiler -l $i.in > $i.res	
+	../debug -l $i.in > $i.res	
 	tmp=$(diff -q -b $i.out $i.res)
 	if [ -z $1 ] && [ "$1" != "n" ]; then echo "$i: $tmp"; fi
 	if [ -n "$tmp" ]; then fail="$fail $i"; fi
